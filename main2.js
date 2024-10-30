7df5cc1d9ffd6ee57bcb121b102f898f9b4eb1da
StefanoMurtas <stefanomurtas16@gmail.com>
2024-10-30 12:52:52 +0100

    add prova2 debug

--- main2.js
+++ main2.js
@@ -24,7 +24,7 @@
 
 
 //     if (valoreSelezione === "backend") {
-        
+
 //         valoreSelezione = sviluppoBackend * oreLavoro;
 //         console.log(valoreSelezione);
 
@@ -48,15 +48,35 @@
 
 // inzio prova 2
 
-function calcoloCosto (){
+document.getElementById("tipoLavoro").addEventListener('change', calcoloCosto);
+function calcoloCosto() {
     // costante tipo lavoro deve contenere l'id html per giungere al primo collegamento
-    const tipoLavoro =document.getElementById("tipoLavoro").value; 
+    const lavoroTipo = document.getElementById("tipoLavoro").value;
     // costante oreLavoro (in questa inseriamo il valore 10 prefissato nella richista dell'esercizio)
-    const oreLavoro =10;
+    const oreLavoro = 10;
     // pagaOraia (variabile vuota per contenere il valore di un ora di lavoro per ogni tipologia di servizio)
     let pagaOraria = 0;
+
+    // logica 1 (se il lavoro selezionato è frontend l'importo sarà ..)
+    if (lavoroTipo === "frontend") {
+        pagaOraria = 22.50;
+        // logica 2(se il lavoro selezionato è backend l'importo sarà ..)
+    } else if (lavoroTipo === "backEnd") {
+        pagaOraria = 15.30;
+        // logica 3 (se il lavoro selezionato è analisiProgettuale l'importo sarà ..)
+    } else if (lavoroTipo === "analisiProgettuale") {
+        pagaOraria = 33.60;
+        // logica 4 (nel caso nela quale non fosse stato selezionata nessuna delle 3 messaggio "selezionare un tipo di lavoro anzichè il risulato")
+    } else {
+        document.getElementById("risultato").innerHTML = "selezionare un tipo di lavoro";
+        return;
+    }
+    // creata nuova variabile per contenere il risulato della moltiplicazione tra ore e tipo lavoro selezionato
+    const spesaTotale = oreLavoro * pagaOraria;
+    // riceercare il risultato nel html e creare il risultato al suo interno
+    document.getElementById("risultato").innerText = `il totale è €${spesaTotale.toFixed(2)}`;
+
+
 }
-if (tipoLavoro === "frontend" ){
-    pagaOraria= 22.50;
-    console.log (pagaOraria)
-}
+
+
