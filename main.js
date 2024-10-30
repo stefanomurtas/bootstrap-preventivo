b816434cc6a9e5c96cef151ca292db44978ac7c7
StefanoMurtas <stefanomurtas16@gmail.com>
2024-10-30 12:00:51 +0100

    add function for the html select

---
+++ main.js
@@ -0,0 +1,40 @@
+function usePromotionalCode() {
+    const promtionalCodeValue = $('#nomeUtente').value;
+     const tipoLavoroVAlue= $('#tipoLavoro').value;
+}
+
+// 1prendo valore codice promozionale
+// 2 prendo tipo di lavoro
+// 3 tarifa base  a seconda lavoro
+
+
+//  creare condizione per dare il valore di prezzo al tipo di lavoro
+// aggiungere valore per il numero delle ore che in questo caso sarà 10 
+// aggiungere tutte variabili che potrebbero essere scelte dal cliente
+let sviluppoBackend = 22.50;
+let sviluppoFrontend = 15.30;
+let analisiProgett = 33.60;
+
+// const oreLavoro = 10
+// document.getElementById('tipoLavoro').addEventListener('click', function sceltaLavoro() {
+//     const tipoLavoroValue = ('')
+
+//     if (tipoLavoroValue === "backend") {
+//         tipoLavoroValue = sviluppoBackend * oreLavoro;
+//         alert("prova")
+//     } else if (tipoLavoroValue === "frontend") {
+//         tipoLavoroValue = sviluppoFrontend * oreLavoro;
+//         alert("prova")
+
+
+//     } else (tipoLavoroValue === "analisiProgettuale"){
+//         tipoLavoroValue = analisiProgett * oreLavoro
+//         alert("prova")
+
+//     }
+
+// })
+// console.log = (tipoLavoroValue)
+
+
+
---
+++ main2.js
@@ -0,0 +1,53 @@
+// 1prendo valore codice promozionale
+// 2 prendo tipo di lavoro
+// 3 tarifa base  a seconda lavoro
+//  creare condizione per dare il valore di prezzo al tipo di lavoro
+// aggiungere valore per il numero delle ore che in questo caso sarà 10 
+// aggiungere tutte variabili che potrebbero essere scelte dal cliente
+
+// ricerco la select con id
+
+
+const elementoSelezionato = document.getElementById('tipoLavoro');
+
+
+// ricevo il valore iniziale della select (Seleziona il tipo di lavoro)
+// console.log(mySelect.value)
+
+elementoSelezionato.addEventListener('change', function () {
+    let sviluppoBackend = 22.50;
+    let sviluppoFrontend = 15.30;
+    let analisiProgetto = 33.60;
+    let oreLavoro = 10;
+
+    const valoreSelezione = Number(elementoSelezionato.value);
+
+
+    if (valoreSelezione === "backend") {
+        
+        valoreSelezione = sviluppoBackend * oreLavoro;
+        console.log(valoreSelezione);
+
+    } else if (valoreSelezione === "frontend")
+    {
+        valoreSelezione = sviluppoFrontend * oreLavoro;
+        console.log(valoreSelezione);
+    } 
+    else if (valoreSelezione === "analisiProgettuale")
+    {
+        valoreSelezione = analisiProgetto * oreLavoro;
+        console.log(valoreSelezione);
+    }
+    else {
+        document.getElementById("result").innerHTML ="selezionare un tipo di lavoro";
+        return;
+    }
+}
+)
+// codice non funzionante
+
+// inzio prova 2
+
+function calcoloCosto (){
+    const 
+}
