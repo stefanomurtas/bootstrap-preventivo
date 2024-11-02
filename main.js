73b9b0200722a75ef69d153bf0106d0f49afec8b
StefanoMurtas <stefanomurtas16@gmail.com>
2024-11-02 15:48:02 +0100

    add footer and ms-auto in the ul

--- main.js
+++ main.js
@@ -38,3 +38,57 @@ let analisiProgett = 33.60;
 
 
 
+// prova
+
+
+// function Modulo() {
+// let nome= document.getElementById("nomeUtente").value;
+
+
+// if ((nome == "") || (nome == "undefined")) {
+//     alert("Il campo Nome è obbligatorio.");
+//     document.modulo.nome.focus();
+//     return false;
+//     }
+// }
+
+const formCalcolo= document.getElementById("calcolo");
+
+
+
+document.getElementById("calcolo").addEventListener('click', function (event){
+
+    event.preventDefault();
+
+ calcoloCosto() 
+
+})
+
+
+function calcoloCosto() {
+    
+    // costante tipo lavoro deve contenere l'id html per giungere al primo collegamento
+    const lavoroTipo = document.getElementById("tipoLavoro").value;
+    // costante oreLavoro (in questa inseriamo il valore 10 prefissato nella richista dell'esercizio)
+    const oreLavoro = 10;
+    // pagaOraia (variabile vuota per contenere il valore di un ora di lavoro per ogni tipologia di servizio)
+    let pagaOraria = 0;
+
+    // logica 1 (se il lavoro selezionato è frontend l'importo sarà ..)
+    if (lavoroTipo === "frontend") {
+        pagaOraria = 22.50;
+        // logica 2(se il lavoro selezionato è backend l'importo sarà ..)
+    } else if (lavoroTipo === "backend") {
+        pagaOraria = 15.30;
+        // logica 3 (se il lavoro selezionato è analisiProgettuale l'importo sarà ..)
+    } else if (lavoroTipo === "analisiProgettuale") {
+        pagaOraria = 33.60;
+        // logica 4 (nel caso nela quale non fosse stato selezionata nessuna delle 3 messaggio "selezionare un tipo di lavoro anzichè il risulato")
+    } else {
+        window.alert("selezionare un tipo di lavoro")
+    }
+    // creata nuova variabile per contenere il risulato della moltiplicazione tra ore e tipo lavoro selezionato
+    const spesaTotale = oreLavoro * pagaOraria;
+    // riceercare il risultato nel html e creare il risultato al suo interno
+    document.getElementById("risultato").innerText = ` €${spesaTotale.toFixed(2)}`;
+}
