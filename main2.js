0065ea5f577c64f1dbad576353ca81e78496010f
StefanoMurtas <stefanomurtas16@gmail.com>
2024-11-20 14:34:23 +0100

    prova x github

--- main2.js
+++ main2.js
@@ -1,21 +1,21 @@
 
 
-let spesaTotale=0;
+let spesaTotale = 0;
 
 // assegnato a formCalcolo l'id del form
-const formCalcolo= document.getElementById("calcolo");
+const formCalcolo = document.getElementById("calcolo");
 // all'id assegno l'evento dopo il click sul bottone la funzione che mi permette di non riagiornare la pagina
-formCalcolo.addEventListener('submit', function (event){
+formCalcolo.addEventListener('submit', function (event) {
 
     event.preventDefault();
 
- calcoloCosto();
+    calcoloCosto();
 
 
 })
 
 function calcoloCosto() {
-    
+
     // costante tipo lavoro deve contenere l'id html per giungere al primo collegamento
     const lavoroTipo = document.getElementById("tipoLavoro").value;
     // costante oreLavoro (in questa inseriamo il valore 10 prefissato nella richista dell'esercizio)
@@ -40,23 +40,28 @@ function calcoloCosto() {
     }
     // creata nuova variabile per contenere il risulato della moltiplicazione tra ore e tipo lavoro selezionato
     spesaTotale = oreLavoro * pagaOraria;
-//   costante codice promozionale deve contenere l'id html per giungere al collegamento
-    const codPromzionale= document.getElementById("codPromo").value;
-// codice sconto validi
- const codiciSc =[ "YHDNU32" , " JANJC63" , "SJDP096" ] ;
-// creato nuova variabile contenente il valore totale del calcolo precedente
- let prezzoFinale =spesaTotale;
-// logica nella quale si ricerca nella input text un codice contenuto nell' array
-if (codiciSc.includes(codPromzionale)){
-// calcolo per lo sconto in caso il codice sia valido
-    prezzoFinale *= 0.75;
-  }else{
-    // finestra di uscita in caso non ci sia un codice valido
-    window.alert("inserire codice sconto valido");
-  }
-  // riceercare il risultato nel html e creare il risultato al suo interno
-  document.getElementById("risultato").innerText = ` il totale è €${prezzoFinale.toFixed(2)}`;
-  
+    //   costante codice promozionale deve contenere l'id html per giungere al collegamento
+    const codPromzionale = document.getElementById("codPromo").value;
+    // codice sconto validi
+    const codiciSc = ["YHDNU32", " JANJC63", "SJDP096"];
+    // creato nuova variabile contenente il valore totale del calcolo precedente
+    let prezzoFinale = spesaTotale;
+    // logica nella quale si ricerca nella input text un codice contenuto nell' array (doppia condizione per fare in modo che anche nel momento nella quale non fosse inserito il codice sconto il calcolo non si blocchi)
+    if (codPromzionale) {
+        if (codiciSc.includes(codPromzionale)) {
+            // calcolo per lo sconto in caso il codice sia valido
+            prezzoFinale *= 0.75;
+
+        } else {
+            // finestra di uscita in caso non ci sia un codice valido
+            window.alert("inserire codice sconto valido");
+            return;
+
+        }
+    }
+    // riceercare il risultato nel html e creare il risultato al suo interno
+    document.getElementById("risultato").innerText = ` il totale è €${prezzoFinale.toFixed(2)}`;
+
 }
 
 
